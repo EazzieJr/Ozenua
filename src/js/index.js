d64ca120e21f23a179ac4053b9e62f851b88db37
Ganiyu Idris <golamide27@gmail.com>
2021-11-04 23:47:02 +0100

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+// import '../../fonts/stylesheet.css'
+// import '../../src/sass/style.scss'
+// import '../../src/sass/cursor.scss'
+// // import '../../src/sass/texts.scss'
+// import '../../src/public/styles/style.css'
+
+import Cursor from "./cursor.js";
+
+const cursor = new Cursor(document.querySelector(".cursor"));
---
+++ tailwind.config.js
@@ -0,0 +1,70 @@
+module.exports = {
+  // mode: 'jit',
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/sass/*.css',
+    './fonts/euclid/*.css'
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    screens: {
+      'xxs': '375px',
+      'xs': '411px',
+      'sm': '540px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1200px',
+      '2xl': '1536px',
+    },
+
+    fontSize: {
+      'tiny': '.6rem',
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      'lg3xl': '2rem',
+      '4xl': '2.25rem',
+      'lg4xl': '2.5rem',
+      '5xl': '3rem',
+      'lg5xl': '3.5rem',
+      '6xl': '4rem',
+      'lg6xl': '4.5rem',
+      '7xl': '5rem',
+      'lg7xl': '5.5rem',
+      '8xl': '6rem',
+      '9xl': '7rem',
+    },
+
+    extend: {
+      fontFamily: {
+        sarala: ['Sarala'],
+        jolly: ['Jolly Lodger'],
+        montserrat: ['Montserrat'],
+        alternative: ['Montserrat Alternatives'],
+        quicksand: ['Quicksand'],
+        cabin: ['Cabin'],
+        rubik: ['Rubik'],
+        manrope: ['Manrope'],
+        josefin: ['Josefin Sans'],
+        caudex: ['Caudex'],
+        roboto: ['Roboto'],
+        opensans: ['Open Sans']
+      },
+
+      textColor: {
+        // 'primary': '#3490dc',
+        'light': '#F1F1F1',
+        'dark': '#18161D',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
